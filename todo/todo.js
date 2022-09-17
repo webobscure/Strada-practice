ecbd1ce8ff73bef1f4d37014013a46d4ba67e527
webobscure <sparkygino@gmail.com>
2022-09-17 11:59:54 +0300

    make todo

--- todo/todo.js
+++ todo/todo.js
@@ -1,43 +1,139 @@
 const ELEMENTS = {
-    addTaskHigh : document.getElementById('add_task_high'),
-    addTaskLow : document.getElementById('add_task_low'),
-    listHigh : document.querySelector('.list_high li:first-child'),
-    listLow : document.querySelector('.list_low li:first-child'),
+    addTaskHigh : document.querySelector('.add_task_high'),
+    addTaskLow : document.querySelector('.add_task_low'),
+    highInput : document.querySelector('.high_input'),
+    lowInput : document.querySelector('.low_input'),
+    listHigh : document.querySelector('.list_high'),
+    listLow : document.querySelector('.list_low')
 }
 
-function addTask () {
+const PRIORITY = {
+    high: 'high',
+    low: 'low',
+}
 
-let addNewTaskHigh = ELEMENTS.addTaskHigh.value;
-let addNewTaskLow = ELEMENTS.addTaskLow.value;
+const STATUS = {
+    toDo : 'status_todo',
+    done : 'status_done',
+}
 
-if(addNewTaskHigh != '') {
-    ELEMENTS.listHigh.insertAdjacentHTML('afterend',
-    `<li>
-        <input type="checkbox" id="first_task_high" name="to_do">
-            <label for="first_task_high">
-                <p>
-                ${addNewTaskHigh}
-                </p>
-            </label>
-                <button class="btn_exit" type="button"></button>
+
+const list = [ { name: 'Изучить новую тему', priority: PRIORITY.high, status: STATUS.toDo},
+               { name: 'Сверстать этот TODO list', priority: PRIORITY.high, status: STATUS.toDo},
+               { name: 'Начать делать задачу', priority: PRIORITY.high, status: STATUS.toDo},
+               { name: 'Посмотреть ютубчик', priority: PRIORITY.low, status: STATUS.toDo},];
+
+
+// Add new task
+
+function addTask (event, newTask, priority) {
+    try {
+        if(newTask.value.trim() != '' && list.findIndex (function (item) {return item.name == newTask.value; }) == -1) {
+            list.push({'name': newTask.value, 'priority': priority, 'status' : STATUS.toDo});
+        };
+    event.preventDefault();
+    newTask.value = '';
+    render();
+    } catch(err) {
+        alert(`Ошибка: ${err.message}`);
+    };
+};
+
+// Add listener for addTask
+
+ELEMENTS.highInput.addEventListener('submit', (event)=> {
+    addTask(event, ELEMENTS.addTaskHigh, PRIORITY.high);  
+});
+
+ELEMENTS.lowInput.addEventListener('submit', (event)=> {
+    addTask(event, ELEMENTS.addTaskLow, PRIORITY.low);
+});
+
+
+// Remove selected task
+
+function deleteTask(task) {
+    try{
+    if(list.findIndex (function (item) {return item.name ===  task; }) !== -1) {
+        let deleteItem = list.findIndex(function (item) {
+            return item.name === task;  
+        });
+        list.splice(deleteItem, 1);
+    };
+    render();
+    }catch(err){
+        alert(`Ошибка: ${err.message}`);
+    };
+};
+
+
+// Change task status
+
+function changeStatus(task) { 
+    try{
+    if(list.findIndex (function (item) {return item.name ===  task; }) !== -1) {
+        let changeIndex = list.find(function (item) {
+            return (item.name == task); 
+        });
+        if(changeIndex.status == STATUS.toDo){
+         changeIndex.status = STATUS.done;
+        } else {
+            changeIndex.status = STATUS.toDo; 
+        }
+    }
+    render();
+    }catch(err) {
+        alert(`Ошибка: ${err.message}`);
+    };
+};
+
+// Page update
+
+function render () {
+    try{
+    let delTasks = document.querySelectorAll('.task_todo');
+    delTasks.forEach((item) => {
+        item.remove();
+    });
+
+   for (let item of list) {
+        
+    if(item.priority === PRIORITY.high) {
+    ELEMENTS.listHigh.insertAdjacentHTML('beforeend', 
+    `<li class="task_todo">
+    <label>
+    <input type="checkbox" name="to_do" onclick = 'changeStatus("${item.name}")' ${(item.status == STATUS.done) ? 'checked' : ''}>
+      <p class="task_name">
+        ${item.name}
+      </p>
+    </label>
+      <button class="btn_exit" type="button" onclick = 'deleteTask("${item.name}")'></button>
     </li>`
-     );  
-};
-
-if(addNewTaskLow != '' ) {
-    ELEMENTS.listLow.insertAdjacentHTML('afterend',
-    `<li>
-        <input type="checkbox" id="first_task_high" name="to_do">
-            <label for="first_task_high">
-                <p>
-                ${addNewTaskLow}
-                </p>
-            </label>
-                <button class="btn_exit" type="button"></button>
+     )};
+    
+    if(item.priority === PRIORITY.low) {
+    ELEMENTS.listLow.insertAdjacentHTML('beforeend', 
+    `<li class="task_todo">
+      <label>
+        <input type="checkbox" name="to_do" onclick = 'changeStatus("${item.name}")' ${(item.status == STATUS.done) ? 'checked' : ''}>
+          <p class="task_name">
+            ${item.name}
+          </p>
+      </label>
+        <button class="btn_exit" type="button" onclick = 'deleteTask("${item.name}")'></button>
     </li>`
-     );  
+    )};
+    };
+
+for (let item of document.querySelectorAll('input[type=checkbox]')) {
+    if (item.checked) {
+        let li = item.parentNode.parentNode;
+        li.classList.add('status_done');
+}
+}
+    }catch(err){
+        alert(`Ошибка: ${err.message}`);
+    };
 };
 
-ELEMENTS.addTaskHigh.value = '';
-ELEMENTS.addTaskLow.value = '';
-};
+render();
