75a07b69165a9297d55cfe47ff89815051f190b8
webobscure <sparkygino@gmail.com>
2022-10-09 17:56:44 +0300

    #60 favorites

--- weather/main.js
+++ weather/main.js
@@ -4,24 +4,34 @@ const ELEMENTS = {
     TEMPERATURE: document.querySelector('.now__temperature'),
     ICON_NOW: document.querySelector('.now__icon-img'),
     CITY_NOW: document.querySelector('.now__city'),
+    DELETE_LOCATION: document.querySelectorAll('.locations__delete'),
+    ADD_LOCATION: document.querySelector('.now__heart'),
+    LIST_LOCATION: document.querySelector('.list'),
+    NOW_CITY_NAME: document.querySelector('.now__city-name'),
+    LIST_CITY: document.querySelectorAll('.list-item'),
+    BODY: document.body,
   };
   const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
   const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+  let CITY = ['Amur', 'Samara', 'Bali'];
+  
   ELEMENTS.BTN.addEventListener('click', function (event) {
     event.preventDefault();
     let cityName = ELEMENTS.INPUT.value;
-    let urlTemperature = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-    checkCityName(cityName, urlTemperature);
+    let url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+    checkCityName(cityName, url);
   });
-  function checkCityName(cityName, urlTemperature) {
+  
+  function checkCityName(cityName, url) {
     if (!cityName || !isNaN(cityName)) {
       alert('Enter a correct city');
     } else {
-      changeNow(urlTemperature);
+      changeNow(url);
     }
   }
-  function changeNow(urlTemperature) {
-    fetch(urlTemperature)
+  
+  function changeNow(url) {
+    fetch(url)
       .then((response) => {
         if (!response.ok) {
           throw new Error('data not received from the server');
@@ -29,11 +39,84 @@ const ELEMENTS = {
         return response.json();
       })
       .then((result) => {
-        ELEMENTS.CITY_NOW.textContent = result.name;
+        ELEMENTS.NOW_CITY_NAME.textContent = result.name;
         ELEMENTS.TEMPERATURE.textContent = Math.round(result.main.temp) + '°';
         let iconCode = result.weather[0].icon;
         let urlWeather = ` https://openweathermap.org/img/wn/${iconCode}@2x.png`;
         ELEMENTS.ICON_NOW.src = urlWeather;
       })
       .catch(alert);
+  }
+  
+  ELEMENTS.BODY.onload = function () {
+    if (localStorage.getItem('cityes')) {
+      CITY = JSON.parse(localStorage.getItem('cityes'));
     }
+    getLocalStorageCityes();
+    getLocalStorageCurrentCity();
+  };
+  
+  ELEMENTS.ADD_LOCATION.addEventListener('click', () => addCityArray());
+  
+  function addCityArray() {
+    let nowCity = ELEMENTS.NOW_CITY_NAME.textContent;
+    let pos = CITY.findIndex((city) => city === nowCity);
+    if (pos === -1) {
+      CITY.push(nowCity);
+    } else {
+      alert('City already added');
+    }
+    saveLocalStorageCityes();
+    getLocalStorageCityes();
+  }
+  
+  function saveLocalStorageCityes() {
+    console.log(CITY);
+    localStorage.setItem('cityes', JSON.stringify(CITY));
+  }
+  function getLocalStorageCityes() {
+    if (localStorage.getItem('cityes')) {
+      let cityes = localStorage.getItem('cityes');
+      cityes = JSON.parse(cityes);
+      renderLocation(cityes);
+    }
+  }
+  
+  function getLocalStorageCurrentCity() {
+    let currentCity = localStorage.getItem('currentCity');
+    currentCity = JSON.parse(currentCity);
+    let url = `${serverUrl}?q=${currentCity}&appid=${apiKey}&units=metric`;
+    changeNow(url);
+  }
+  
+  function renderLocation(cityes) {
+    document.querySelectorAll('.list-item').forEach(function (city) {
+      city.remove();
+    });
+  
+    cityes.forEach((city) => {
+      let li = document.createElement('li');
+      li.className = 'list-item';
+      ELEMENTS.LIST_LOCATION.prepend(li);
+      let p = document.createElement('p');
+      p.className = 'list-item-city';
+      p.textContent = city;
+      li.append(p);
+      let btn = document.createElement('button');
+      btn.className = 'locations__delete';
+      btn.textContent = 'X';
+      li.append(btn);
+      li.addEventListener('click', () => {
+        let url = `${serverUrl}?q=${p.textContent}&appid=${apiKey}&units=metric`;
+        changeNow(url);
+        localStorage.setItem('currentCity', JSON.stringify(p.textContent));
+        getLocalStorageCurrentCity();
+      });
+      btn.addEventListener('click', () => {
+        let deleteCity = JSON.parse(localStorage.getItem('cityes'));
+        let newCityes = deleteCity.filter((city) => city !== p.textContent);
+        localStorage.setItem('cityes', JSON.stringify(newCityes));
+        li.remove();
+      });
+    });
+  }
