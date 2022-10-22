b560a4402d35d6c8f3d8fb529a5209d3455fa67d
webobscure <sparkygino@gmail.com>
2022-10-22 11:02:40 +0300

    Weather Recursion

--- weather/main.js
+++ weather/main.js
@@ -1,6 +1,6 @@
 import { ELEMENTS, serverUrl, apiKey, mounths, urlForecast } from './value.js';
 
-let city = new Set(['Moscow', 'New York', 'San Diego'])
+let city = new Set(['San Diego', 'New York', 'Los Altos']);
 
 class ServerError extends Error {
   constructor(message) {
@@ -21,16 +21,15 @@ function checkCityName(cityName, url) {
   if (!cityName || !isNaN(cityName)) {
     alert('Enter a correct city');
     return;
-  } else {
-    checkUrl(url);
-    changeDetails(url);
   }
+  checkUrl(url);
+  changeForecast(cityName);
 }
 
 async function checkUrl(url) {
   try {
     let response = await fetch(url);
-    if(!response.ok) {
+    if (!response.ok) {
       throw new ServerError('data not received from the server');
     }
     let result = await response.json();
@@ -38,14 +37,12 @@ async function checkUrl(url) {
     changeNow(result);
   } catch (err) {
     if (err instanceof ServerError) {
-      alert(err.message)
+      alert(err.message);
     } else {
-      throw new ServerError
+      throw err;
     }
   }
 }
-
-async function change
 
 function changeNow(result) {
   ELEMENTS.NOW_CITY_NAME.textContent = result.name;
@@ -57,8 +54,9 @@ function changeNow(result) {
 
 ELEMENTS.BODY.onload = function () {
   if (localStorage.getItem('cityes')) {
-    CITY = JSON.parse(localStorage.getItem('cityes'));
+    city = new Set(JSON.parse(localStorage.getItem('cityes')));
   }
+  saveLocalStorageCityes();
   getLocalStorageCityes();
   getLocalStorageCurrentCity();
 };
@@ -67,25 +65,20 @@ ELEMENTS.ADD_LOCATION.addEventListener('click', () => addCityArray());
 
 function addCityArray() {
   let nowCity = ELEMENTS.NOW_CITY_NAME.textContent;
-  let pos = CITY.findIndex((city) => city === nowCity);
-  if (pos === -1) {
-    CITY.push(nowCity);
-  } else {
-    alert('City already added');
-  }
+  city.add(nowCity);
   saveLocalStorageCityes();
   getLocalStorageCityes();
 }
 
 function saveLocalStorageCityes() {
-  localStorage.setItem('cityes', JSON.stringify(CITY));
+  localStorage.setItem('cityes', JSON.stringify([...city]));
 }
 
 function getLocalStorageCityes() {
   if (localStorage.getItem('cityes')) {
     let cityes = localStorage.getItem('cityes');
     cityes = JSON.parse(cityes);
-    renderLocation(cityes);
+    renderLocation(cityes, cityes.length - 1);
   }
 }
 
@@ -93,68 +86,70 @@ function getLocalStorageCurrentCity() {
   let currentCity = localStorage.getItem('currentCity');
   currentCity = JSON.parse(currentCity);
   let url = `${serverUrl}?q=${currentCity}&appid=${apiKey}&units=metric`;
-  changeNow(url);
-  changeDetails(url);
+  checkUrl(url);
+  changeForecast(currentCity);
 }
 
-function renderLocation(cityes) {
-  document.querySelectorAll('.list-item').forEach(function (city) {
-    city.remove();
+function renderLocation(cityes, index) {
+  deletelistItem(document.querySelectorAll('.list-item'));
+
+  if (index === 0) return index;
+
+  let newIndex = renderLocation(cityes, index - 1) + 1;
+  let city = cityes[newIndex];
+
+  let li = document.createElement('li');
+  li.className = 'list-item';
+  ELEMENTS.LIST_LOCATION.prepend(li);
+
+  let p = document.createElement('p');
+  p.className = 'list-item-city';
+  p.textContent = city;
+  li.append(p);
+
+  let btn = document.createElement('button');
+  btn.className = 'locations__delete';
+  btn.textContent = 'X';
+  li.append(btn);
+
+  li.addEventListener('click', () => {
+    changeCurrentCity(p.textContent);
   });
 
-  cityes.forEach((city) => {
-    let li = document.createElement('li');
-    li.className = 'list-item';
-    ELEMENTS.LIST_LOCATION.prepend(li);
-    let p = document.createElement('p');
-    p.className = 'list-item-city';
-    p.textContent = city;
-    li.append(p);
-    let btn = document.createElement('button');
-    btn.className = 'locations__delete';
-    btn.textContent = 'X';
-    li.append(btn);
-    li.addEventListener('click', () => {
-      changeCurrentCity(p.textContent);
-    });
-    btn.addEventListener('click', () => {
-      deleteCity(p.textContent, li);
-    });
+  btn.addEventListener('click', () => {
+    deleteCity(p.textContent, li);
+  });
+
+  return newIndex;
+}
+
+function deletelistItem(items) {
+  items.forEach(function (city) {
+    city.remove();
   });
 }
 
 function changeCurrentCity(nameCity) {
   let url = `${serverUrl}?q=${nameCity}&appid=${apiKey}&units=metric`;
-  changeNow(url);
-  changeDetails(url);
+  checkUrl(url);
   localStorage.setItem('currentCity', JSON.stringify(nameCity));
   getLocalStorageCurrentCity();
 }
 
 function deleteCity(nameCity, li) {
-  let deleteCity = JSON.parse(localStorage.getItem('cityes'));
-  let newCityes = deleteCity.filter((city) => city !== nameCity);
-  localStorage.setItem('cityes', JSON.stringify(newCityes));
+  let deleteCity = new Set(JSON.parse(localStorage.getItem('cityes')));
+  deleteCity.delete(nameCity);
+  localStorage.setItem('cityes', JSON.stringify([...deleteCity]));
   li.remove();
 }
 
-function changeDetails(url) {
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('data not received from the server');
-      }
-      return response.json();
-    })
-    .then((result) => {
-      ELEMENTS.DETAILS_CITY.textContent = result.name;
-      changeTemperature(ELEMENTS.DETAILS_TEMPERATURE, 'Temperature: ', result.main.temp);
-      changeTemperature(ELEMENTS.DETAILS_FEELS_LIKE, 'Feels like: ', result.main.feels_like);
-      ELEMENTS.DETAILS_WEATHER.textContent = 'Weather: ' + result.weather[0].main;
-      changeSunriseSunset(result.sys.sunrise, 'Sunrise: ', ELEMENTS.DETAILS_SUNRISE);
-      changeSunriseSunset(result.sys.sunset, 'Sunset: ', ELEMENTS.DETAILS_SUNSET);
-    })
-    .catch(alert);
+function changeDetails(result) {
+  ELEMENTS.DETAILS_CITY.textContent = result.name;
+  changeTemperature(ELEMENTS.DETAILS_TEMPERATURE, 'Temperature: ', result.main.temp);
+  changeTemperature(ELEMENTS.DETAILS_FEELS_LIKE, 'Feels like: ', result.main.feels_like);
+  ELEMENTS.DETAILS_WEATHER.textContent = 'Weather: ' + result.weather[0].main;
+  changeSunriseSunset(result.sys.sunrise, 'Sunrise: ', ELEMENTS.DETAILS_SUNRISE);
+  changeSunriseSunset(result.sys.sunset, 'Sunset: ', ELEMENTS.DETAILS_SUNSET);
 }
 
 function changeTemperature(elementTemperature, textTemperature, resultTemp) {
@@ -165,4 +160,70 @@ function changeSunriseSunset(time, timeOfDay, elementsTimeOfDay) {
   let resultTime = new Date(time * 1000);
   resultTime = resultTime.toLocaleTimeString();
   elementsTimeOfDay.textContent = timeOfDay + resultTime.slice(0, -3);
+}
+
+async function changeForecast(cityName) {
+  deletelistItem(document.querySelectorAll('.forecast__item'));
+  let serverForecast = `${urlForecast}?q=${cityName}&cnt=3&appid=${apiKey}&units=metric`;
+  try {
+    let response = await fetch(serverForecast);
+
+    if (!response.ok) {
+      throw new ServerError('data not received from the server');
+    }
+
+    let result = await response.json();
+    ELEMENTS.FORECAST_CITY.textContent = result.city.name;
+    result.list.map((itemList) => setForecastItem(itemList));
+  } catch (err) {
+    if (err instanceof ServerError) {
+      alert(err.message);
+    } else {
+      throw err;
+    }
+  }
+}
+
+function setForecastItem(itemList) {
+  let nowDate = new Date(itemList.dt * 1000);
+
+  let div = document.createElement('div');
+  div.className = 'forecast__item';
+  ELEMENTS.FORECAST_LIST.prepend(div);
+
+  let divItem = document.createElement('div');
+  divItem.className = 'forecast__time';
+  div.append(divItem);
+
+  let spanDate = document.createElement('span');
+  spanDate.textContent = nowDate.getDate() + ' ' + mounths[nowDate.getMonth()];
+  divItem.append(spanDate);
+
+  let spanTime = document.createElement('span');
+  spanTime.textContent = nowDate.toLocaleTimeString().slice(0, -3);
+  divItem.append(spanTime);
+
+  let divWeather = document.createElement('div');
+  divWeather.className = 'forecast__weather';
+  div.append(divWeather);
+
+  let divTemperature = document.createElement('div');
+  divTemperature.className = 'forecast__temperature';
+  divWeather.append(divTemperature);
+
+  let spanTemperature = document.createElement('span');
+  spanTemperature.textContent = 'Temperature:' + ' ' + Math.round(itemList.main.temp) + '°';
+  divTemperature.append(spanTemperature);
+
+  let spanFeelsLike = document.createElement('span');
+  spanFeelsLike.textContent = 'Feels like:' + ' ' + Math.round(itemList.main.feels_like);
+  divTemperature.append(spanFeelsLike);
+
+  let divIcon = document.createElement('div');
+  divIcon.className = 'forecast__icon';
+  divWeather.append(divIcon);
+
+  let spanIcon = document.createElement('span');
+  spanIcon.textContent = itemList.weather[0].main;
+  divIcon.append(spanIcon);
 }
